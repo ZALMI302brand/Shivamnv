9ddf2bc68d3a95ee4c8161fcf5b48f61fed0227b
yuvavikashyogna <shivamhg186@gmail.com>
2026-04-27 19:10:14 +0530

    firebase.js

---
+++ firebase.js
@@ -0,0 +1,12 @@
+// Firebase Config (अपना config डालना)
+const firebaseConfig = {
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "YOUR_ID",
+  storageBucket: "YOUR_BUCKET",
+  messagingSenderId: "YOUR_MSG_ID",
+  appId: "YOUR_APP_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
